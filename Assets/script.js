1a8ff3961a854069e9a9aa253b0a14ddad9df64f
Johnny McGown <jmac3292@gmail.com>
2023-02-07 22:30:32 -0700

    attempt to use 3rd api

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,7 @@
-var currentWeatherApiKey = "4147dec85cccdda8f40665aca82055da"
+var currentWeatherApiKey = "8b761f33816e95be99e5d105fa06bc99"
 
-var searchForm = document.querySelector(".citySearch")
-var searchCity = document.querySelector("#inputCity")
+var searchForm = document.querySelector(".citySearch");
+var searchCity = document.querySelector("#inputCity");
 
 let citySearch = function(event){
     event.preventDefault();
@@ -9,5 +9,26 @@ let citySearch = function(event){
     if (search) {
         console.log(searchCity.value);
     }
+
+    fetch(
+        'https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${currentWeatherApiKey}'
+    )
+    .then(function(response){
+        return response.json();
+    })
+    .then(function (data){
+        console.log((data));
+        getWeather(data.coord.lat, data.coord.lon, data.name, data.weather);
+    })
+    .catch();
 }
+
+function getWeather(latitude, longitude, cityname, currentWeatherData) {
+    console.log(latitude, longitude);
+
+    fetch(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$(latitude)&lon=$(longitude)&appid=$(currentWeatherApiKey)'
+    )
+}
+
 searchForm.addEventListener("click", citySearch)
