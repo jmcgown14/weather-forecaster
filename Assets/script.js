3af8e56246a377e4484621b10c3b24e52c7daa87
Johnny McGown <jmac3292@gmail.com>
2023-03-19 23:34:38 -0600

    Working on changes to resubmit assignment with help from classmate

--- Assets/script.js
+++ Assets/script.js
@@ -1,47 +1,111 @@
 var currentWeatherApiKey = '8b761f33816e95be99e5d105fa06bc99'
+var searchForm = document.querySelector(".inputGroup");
+var searchItem = document.querySelector("#searchItem");
+var currentWeatherEl = document.querySelector(".current");
 
-var searchForm = document.querySelector('#searchBtn');
-var searchCity = document.querySelector("#inputCity");
+// Event Listener
+searchForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log(searchItem.value);
 
-let citySearch = function(event){
-    event.preventDefault();
-    let search=searchCity.value;
-    if (search) {
-        console.log(searchCity.value);
-    }
-    console.log(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${currentWeatherApiKey}`)
+  if (
+    localStorage.getItem("weatherSearchHistory") &&
+    !localStorage.getItem("weatherSearchHistory").includes(searchItem.value)
+  ) {
+    const arrayFromStorage = JSON.parse(localStorage.getItem("weatherSearchHistory"));
+    arrayFromStorage.push(searchItem.value);
+    localStorage.setItem("weatherSearchHistory", JSON.stringify(arrayFromStorage));
+  } else if (!localStorage.getItem("weatherSearchHistory")) {
+    localStorage.setItem("weatherSearchHistory", JSON.stringify([searchItem.value]));
+  }
+populateHistory();
+  weatherData(searchItem.value);
+  currentWeatherEl.style.display= "block"
+});
 
-    fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${currentWeatherApiKey}`
-    )
-    .then(function(response){
-        return response.json();
+var buttonContainer = document.querySelector('#button-container')
+populateHistory();
+function populateHistory(){
+  const arrayFromStorage = JSON.parse(localStorage.getItem("weatherSearchHistory"));
+
+if (arrayFromStorage){
+  // 
+  buttonContainer.innerHTML=''
+  for (var i = 0; i < arrayFromStorage.length; i++){
+    const button = document.createElement('button')
+    button.innerText = arrayFromStorage[i]
+    button.addEventListener('click', (e)=>{
+      weatherData(e.target.textContent)
     })
-    .then(function (data){
-        console.log((data));
-        getWeather(data.coord.lat, data.coord.lon, data.name, data.weather);
+    buttonContainer.append(button)
+  }
+}
+}
+
+function weatherData(searchTerm) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${currentWeatherApiKey}`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log("FIRST RESPONSE", data);
+      getWeather(data.coord.lat, data.coord.lon, data.name, data);
     })
     .catch();
 }
 
-function getWeather(latitude, longitude, cityname, currentWeatherData) {
-    console.log(latitude, longitude);
-
-    fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${currentWeatherApiKey}`
-    )
-    .then(function(response) {
-        return response.json();
+function getWeather(latitude, longitude, cityName, currentWeatherData) {
+  console.log(latitude, longitude);
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=$${currentWeatherApiKey}&units=imperial`
+  )
+    .then(function (response) {
+      return response.json();
     })
-    .then(function (weatherForecast) {
-        renderCurrentForecast(cityName, weatherForecast, currentWeatherData);
+    .then(function (forecastWeather) {
+      renderCurrentWeather(cityName, forecastWeather, currentWeatherData);
     });
 }
 
-function renderCurrentWeather(params) {
-    // need to creat todays weather //
-    // using a for loop to creat the desired results for 5 day forecast//
-}
+function renderCurrentWeather(cityName, forecastWeather, currentWeatherData) {
+  var todayDate = dayjs().format("MM/DD/YYYY");
+  console.log("current: ", currentWeatherData);
 
-// local storage to save cities that you selected with a button
-searchForm.addEventListener("click", citySearch)
+  console.log("forecast: ", forecastWeather.list);
+  var tempKtoF = Math.floor(
+    (currentWeatherData.main.temp - 273.15) * (9 / 5) + 32
+  );
+  // Programatically display that data to the page
+  currentWeatherEl.innerHTML = `
+        <h3>${cityName}</h3>
+                <div>
+                    <h4>${todayDate}</h4>
+                </div>
+                <p>Temperature: ${tempKtoF} °F</p>
+                <p>Wind Speed: ${currentWeatherData.wind.speed} MPH</p>
+                <p>Humidity: ${currentWeatherData.main.humidity}%</p>
+    `;
+
+  renderForecastWeather(forecastWeather);
+}
+// Create function for forecastWeather
+function renderForecastWeather(forecastWeather) {
+  var elements = document.getElementsByClassName("card");
+  // for Loop for the forecast
+  for (var i = 0; i < elements.length; i++) {
+    x = i * 8;
+    let date = dayjs()
+      .add(i + 1, "day")
+      .format("MM/DD/YYYY");
+
+    // Programatically display that data to the page
+    elements[i].innerHTML = `
+              <h3>${date}</h3>
+              <p>Temperature: ${forecastWeather.list[x].main.temp} °F</p>
+              <p>Wind Speed: ${forecastWeather.list[x].wind.speed} MPH</p>
+              <p>Humidity: ${forecastWeather.list[x].main.humidity}%</p>
+              `;
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-I had some perosnl issues come up that led to me not fully being able to complete this challenge. I am just submitting what I was able to get done
+# 06 Server-Side APIs: Weather Dashboard
+
+## Your Task
+```
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+```
+
+## Acceptance Criteria
+
+```
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, the temperature, the humidity, and the wind speed
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+```
+
+## Mock-Up
+
+The following image shows the web application's appearance and functionality:
+
+
+
+
+
+Worked in collaboration with Katherine Renier
