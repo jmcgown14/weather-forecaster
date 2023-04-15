c17ef6efaeaace00ec4ddf74d71f55c09894d7fc
Johnny McGown <jmac3292@gmail.com>
2023-04-15 14:12:47 -0600

    worked with tutor

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,4 @@
-var currentWeatherApiKey = '8b761f33816e95be99e5d105fa06bc99'
+var currentWeatherApiKey = "8b761f33816e95be99e5d105fa06bc99";
 var searchForm = document.querySelector(".inputGroup");
 var searchItem = document.querySelector("#searchItem");
 var currentWeatherEl = document.querySelector(".current");
@@ -12,34 +12,44 @@ searchForm.addEventListener("submit", function (e) {
     localStorage.getItem("weatherSearchHistory") &&
     !localStorage.getItem("weatherSearchHistory").includes(searchItem.value)
   ) {
-    const arrayFromStorage = JSON.parse(localStorage.getItem("weatherSearchHistory"));
+    const arrayFromStorage = JSON.parse(
+      localStorage.getItem("weatherSearchHistory")
+    );
     arrayFromStorage.push(searchItem.value);
-    localStorage.setItem("weatherSearchHistory", JSON.stringify(arrayFromStorage));
+    localStorage.setItem(
+      "weatherSearchHistory",
+      JSON.stringify(arrayFromStorage)
+    );
   } else if (!localStorage.getItem("weatherSearchHistory")) {
-    localStorage.setItem("weatherSearchHistory", JSON.stringify([searchItem.value]));
+    localStorage.setItem(
+      "weatherSearchHistory",
+      JSON.stringify([searchItem.value])
+    );
   }
-populateHistory();
+  populateHistory();
   weatherData(searchItem.value);
-  currentWeatherEl.style.display= "block"
+  currentWeatherEl.style.display = "block";
 });
 
-var buttonContainer = document.querySelector('#button-container')
+var buttonContainer = document.querySelector("#button-container");
 populateHistory();
-function populateHistory(){
-  const arrayFromStorage = JSON.parse(localStorage.getItem("weatherSearchHistory"));
+function populateHistory() {
+  const arrayFromStorage = JSON.parse(
+    localStorage.getItem("weatherSearchHistory")
+  );
 
-if (arrayFromStorage){
-  // 
-  buttonContainer.innerHTML=''
-  for (var i = 0; i < arrayFromStorage.length; i++){
-    const button = document.createElement('button')
-    button.innerText = arrayFromStorage[i]
-    button.addEventListener('click', (e)=>{
-      weatherData(e.target.textContent)
-    })
-    buttonContainer.append(button)
+  if (arrayFromStorage) {
+    //
+    buttonContainer.innerHTML = "";
+    for (var i = 0; i < arrayFromStorage.length; i++) {
+      const button = document.createElement("button");
+      button.innerText = arrayFromStorage[i];
+      button.addEventListener("click", (e) => {
+        weatherData(e.target.textContent);
+      });
+      buttonContainer.append(button);
+    }
   }
-}
 }
 
 function weatherData(searchTerm) {
@@ -59,53 +69,46 @@ function weatherData(searchTerm) {
 function getWeather(latitude, longitude, cityName, currentWeatherData) {
   console.log(latitude, longitude);
   fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=$${currentWeatherApiKey}&units=imperial`
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${currentWeatherApiKey}&units=imperial`
   )
     .then(function (response) {
       return response.json();
     })
     .then(function (forecastWeather) {
-      renderCurrentWeather(cityName, forecastWeather, currentWeatherData);
+      renderCurrentWeather(cityName, currentWeatherData);
+      renderForecastWeather(forecastWeather);
     });
 }
 
-function renderCurrentWeather(cityName, forecastWeather, currentWeatherData) {
+function renderCurrentWeather(currentWeatherData) {
   var todayDate = dayjs().format("MM/DD/YYYY");
   console.log("current: ", currentWeatherData);
 
-  console.log("forecast: ", forecastWeather.list);
-  var tempKtoF = Math.floor(
-    (currentWeatherData.main.temp - 273.15) * (9 / 5) + 32
-  );
   // Programatically display that data to the page
   currentWeatherEl.innerHTML = `
-        <h3>${cityName}</h3>
+        <h3>${currentWeatherData.name}</h3>
                 <div>
                     <h4>${todayDate}</h4>
                 </div>
-                <p>Temperature: ${tempKtoF} °F</p>
+                <p>Temperature: ${currentWeatherData.main.temp} °F</p>
                 <p>Wind Speed: ${currentWeatherData.wind.speed} MPH</p>
                 <p>Humidity: ${currentWeatherData.main.humidity}%</p>
     `;
-
-  renderForecastWeather(forecastWeather);
 }
-// Create function for forecastWeather
-function renderForecastWeather(forecastWeather) {
-  var elements = document.getElementsByClassName("card");
-  // for Loop for the forecast
-  for (var i = 0; i < elements.length; i++) {
-    x = i * 8;
-    let date = dayjs()
-      .add(i + 1, "day")
-      .format("MM/DD/YYYY");
 
-    // Programatically display that data to the page
-    elements[i].innerHTML = `
-              <h3>${date}</h3>
-              <p>Temperature: ${forecastWeather.list[x].main.temp} °F</p>
-              <p>Wind Speed: ${forecastWeather.list[x].wind.speed} MPH</p>
-              <p>Humidity: ${forecastWeather.list[x].main.humidity}%</p>
-              `;
+// Create function for forecastWeather
+const forecastDiv = document.getElementById("forecast");
+function renderForecastWeather(forecastWeather) {
+  //empty forecast div
+  forecastDiv.innerHTML = "";
+  for (let i = 5; i < forecastWeather.list.length; i += 8) {
+    const card = document.createElement("div");
+    console.log(forecastWeather.list[i]);
+    card.innerHTML = ` <h3>${forecastWeather.list[i].dt_txt}</h3>
+               <p>Temperature: ${forecastWeather.list[i].main.temp} °F</p>
+              <p>Wind Speed: ${forecastWeather.list[i].wind.speed} MPH</p>
+             <p>Humidity: ${forecastWeather.list[i].main.humidity}%</p>`;
+
+    forecastDiv.append(card);
   }
 }
